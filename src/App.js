6ff99a9acaeaeb7a011549258e0b8fa394493f90
John Mirschel <john@Johns-MacBook-Air.local>
2019-07-20 13:28:58 -0400

    Added whisper api call, and card styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/Navbar';
@@ -11,21 +13,46 @@ import login from './pages/login';
 import signup from './pages/signup';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <div className='container'>
-            <Switch>
-              <Route  exact path='/' component={home}/>
-              <Route  exact path='/login' component={login}/>
-              <Route  exact path='/signup' component={signup}/>
-            </Switch>
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#fc6f1d',
+      main: '#E35604',
+      dark: '#c94c03',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#03e2e2',
+      main: '#03c9c9',
+      dark: '#03b0b0',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+class App extends Component {
+  render(){
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar/>
+            <div className='container'>
+                <Switch>
+                  <Route  exact path='/' component={home}/>
+                  <Route  exact path='/login' component={login}/>
+                  <Route  exact path='/signup' component={signup}/>
+                </Switch>
+            </div>
+          </Router>
         </div>
-      </Router>
-    </div>
-  );
+      </MuiThemeProvider>
+    );
+  }
+  
 }
 
 export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import Whisper from '../components/Whisper';
 
 export class home extends Component {
+    state= {
+        whispers: null
+    }
+
+    componentDidMount(){
+        axios.get('/whispers')
+            .then(res => {
+                this.setState({
+                    whispers: res.data
+                })
+            })
+            .catch(err => console.log(err));
+    }
     render() {
+        let recentWhsipersMarkup = this.state.whispers ? (
+            this.state.whispers.map(whisper => <Whisper key="whisper.whisperId" whisper={whisper}/>)
+        ) : <p>Loading...</p>
+
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={10}>
+                <Grid item sm={8} xs={12}>
+                    {recentWhsipersMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+          </Grid>
         )
     }
 }
