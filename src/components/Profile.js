cbb68005f9f7eb3daa4e20e115e408c21797f56f
John Mirschel <evan.mirschel@gmail.com>
2019-07-23 17:09:43 -0400

    Added logout and edit profile features

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
+import EditDetails from './EditDetails';
 
 //MUI stuff
 import Button from "@material-ui/core/Button";
@@ -17,6 +18,7 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
 import EditIcon from "@material-ui/icons/Edit";
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
 
 //Redux
 import { connect } from "react-redux";
@@ -83,6 +85,11 @@ class Profile extends Component {
     const fileInput = document.getElementById("imageInput");
     fileInput.click();
   };
+
+  handleLogout = () =>{
+    this.props.logoutUser();
+  }
+
   render() {
     const {
       classes,
@@ -143,6 +150,12 @@ class Profile extends Component {
               <CalendarToday color="primary" />
               <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
             </div>
+            <ToolTip title='Logout' placement="top">
+              <IconButton onClick={this.handleLogout}>
+                <KeyboardReturn color='primary'/>
+              </IconButton>
+            </ToolTip>
+            <EditDetails/>
           </div>
         </Paper>
       ) : (
