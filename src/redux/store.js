3d7c3327f33b2ddb37b516282ea879dbf9199cc7
Benji Mirschel <cliff.mirschel@gmail.com>
2019-08-13 13:15:08 -0400

    Fixed bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import userReducer from './reducers/userReducer';
-import dataReducer from './reducers/dataReducer';
-import uiReducer from './reducers/uiReducer';
+import userReducer from "./reducers/userReducer";
+import dataReducer from "./reducers/dataReducer";
+import uiReducer from "./reducers/uiReducer";
 
 const initialState = {};
 
@@ -20,7 +20,9 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 
